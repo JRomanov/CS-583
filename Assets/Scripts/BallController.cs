ddb446b6836186dac23e369822740ebb5fce4f92
Jesus Sebastian Aviles <javiles3091@sdsu.edu>
2020-12-07 07:34:31 -0800

    Added ball respawn functionality for falling off platform in lvl 1

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     public float ballSpeed;
-    private Rigidbody ballRB;
-    private Collider ballCol;
+    public GameObject ball;
+    public Rigidbody ballRB;
+    public Collider ballCol;
     private bool Paused;
+    public Vector3 checkPoint;
 
     public static BallController Instance { get; private set; }
     public void Awake()
@@ -17,8 +19,12 @@ public class BallController : MonoBehaviour
             //Debug.Log("New Level");
             DontDestroyOnLoad(this);
             Instance = this;
+            ball = GameObject.FindGameObjectWithTag("Player");
             ballRB = GetComponent<Rigidbody>();
             ballCol = GetComponent<Collider>();
+
+            //Level 1 starting position
+            checkPoint = new Vector3(135, 33, -2);
         }
         else
         {
@@ -37,9 +43,24 @@ public class BallController : MonoBehaviour
         float xSpeed = Input.GetAxis("Horizontal");
         float ySpeed = Input.GetAxis("Vertical");
 
-
         ballRB.AddForce(Vector3.ClampMagnitude(new Vector3(-ySpeed, 0, xSpeed), 1f) * ballSpeed * Time.deltaTime, ForceMode.Acceleration);
-        ballRB.AddTorque(Vector3.ClampMagnitude(new Vector3(xSpeed, 0, ySpeed), 1f) * ballSpeed * Time.deltaTime, ForceMode.Acceleration);//balance speed with FPS
-        
+
+        //balance speed with FPS
+        ballRB.AddTorque(Vector3.ClampMagnitude(new Vector3(xSpeed, 0, ySpeed), 1f) * ballSpeed * Time.deltaTime, ForceMode.Acceleration); 
+    }
+    //collectables/Boosters
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    //Collision Tracker --> Enemies/Obstacles/Restarts
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Restart")
+        {
+            ball.transform.position = checkPoint;
+            ballRB.velocity = new Vector3(0, 0, 0);
+        }
     }
 }
