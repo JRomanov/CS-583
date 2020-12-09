8e2f76fe42ded85f9c8f692be6505f0b8adaa14c
Jesus Sebastian Aviles <javiles3091@sdsu.edu>
2020-12-08 17:35:38 -0800

    Created a new Winscreen BG in krita, changed timer functionality

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -10,29 +10,32 @@ public class BallController : MonoBehaviour
     public GameObject ball;
     public Rigidbody ballRB;
     public Collider ballCol;
-    //knockback
-    private float hurtforce = 20f;
-    Vector3 knockBack;
 
     public GameObject pauseMenu;
+    public GameObject timer;
     private bool Paused;
     public Button ResumeButton;
     public Button MenuButton;
     public Button QuitButton;
     public AudioSource igMusic;
     public float timeRemaining;
-    public Text timer;
+    public Text timerText;
     public Text fallText;
-    public Text ExtraTime1;
-    public Text ExtraTime2;
-    public Text ExtraTime3;
+    public Text levelTime1;
+    public Text levelTime2;
+    public Text levelTime3;
     public float temp;
     public int curLvl;
     public Vector3 checkPoint;
     private int fallCtr = 0;
-    public float lvl1;
-    public float lvl2;
-    public float lvl3;
+    //bnonus time
+    public float lvl1b;
+    public float lvl2b;
+    public float lvl3b;
+    //time completed
+    public float lvl1c;
+    public float lvl2c;
+    public float lvl3c;
 
 
     public static BallController Instance { get; private set; }
@@ -50,7 +53,7 @@ public class BallController : MonoBehaviour
             //Level 1 starting position
             checkPoint = new Vector3(.65f, 1f, .5f);
 
-            //igMusic = GameObject.FindGameObjectWithTag("InGameMusic").GetComponent<AudioSource>();
+            //player Pause Menu references
             pauseMenu = GameObject.FindGameObjectWithTag("PauseMenu");
             MenuButton = GameObject.FindGameObjectWithTag("BackButton").GetComponent<Button>();
             MenuButton.onClick.AddListener(() => mainMenu());
@@ -60,11 +63,14 @@ public class BallController : MonoBehaviour
             ResumeButton.onClick.AddListener(() => ResumeGame());
             Paused = false;
             pauseMenu.SetActive(false);
-            timer = GameObject.FindGameObjectWithTag("TimerText").GetComponent<Text>();
+
+            //player UI references
+            timer = GameObject.FindGameObjectWithTag("Timer");
+            timerText = GameObject.FindGameObjectWithTag("TimerText").GetComponent<Text>();
             fallText = GameObject.FindGameObjectWithTag("FallCtr").GetComponent<Text>();
-            ExtraTime1 = GameObject.FindGameObjectWithTag("ExtraTime1").GetComponent<Text>();
-            ExtraTime2 = GameObject.FindGameObjectWithTag("ExtraTime2").GetComponent<Text>();
-            ExtraTime3 = GameObject.FindGameObjectWithTag("ExtraTime3").GetComponent<Text>();
+            levelTime1 = GameObject.FindGameObjectWithTag("ExtraTime1").GetComponent<Text>();
+            levelTime2 = GameObject.FindGameObjectWithTag("ExtraTime2").GetComponent<Text>();
+            levelTime3 = GameObject.FindGameObjectWithTag("ExtraTime3").GetComponent<Text>();
             igMusic = GameObject.FindGameObjectWithTag("igMusic").GetComponent<AudioSource>();
         }
         else
@@ -117,39 +123,52 @@ public class BallController : MonoBehaviour
         }
         if (level == 3)
         {
-            ExtraTime1.text = "";
-            ExtraTime2.text = "";
-            ExtraTime3.text = "";
+            timer.SetActive(true);
+            levelTime1.text = "";
+            levelTime2.text = "";
+            levelTime3.text = "";
             checkPoint = new Vector3(.65f, 1f, .5f);
             ball.transform.position = checkPoint;
-            timeRemaining = 60f;
+            timeRemaining = 105f;
             curLvl = 3;
             igMusic.Play();
         }
         if (level == 4)
         {
-            lvl1 = timeRemaining;
-            ExtraTime1.text = string.Format("+ {0:00}:{1:00}", Mathf.FloorToInt(lvl1 / 60), Mathf.FloorToInt(lvl1 % 60));
+            //time completed previous level
+            lvl1c = 105f - timeRemaining;
+            //bonus time
+            lvl1b = timeRemaining;
+            levelTime1.text = string.Format("{0:00}:{1:00}", Mathf.FloorToInt(lvl1c / 60), Mathf.FloorToInt(lvl1c % 60));
 
             checkPoint = new Vector3(2.5f, 2f, -2.5f);
             ball.transform.position = checkPoint;
-            timeRemaining = 120f + lvl1;
+            //new timer for current lvl
+            timeRemaining = 120f + timeRemaining;
             curLvl = 4;
         }
         if (level == 5)
         {
-            lvl2 = timeRemaining;
-            ExtraTime2.text = string.Format("+ {0:00}:{1:00}", Mathf.FloorToInt(lvl2 / 60), Mathf.FloorToInt(lvl2 % 60));
+            //time completed previous level
+            lvl2c = 120f - timeRemaining;
+            //bonus time
+            lvl2b = timeRemaining;
+            levelTime2.text = string.Format("{0:00}:{1:00}", Mathf.FloorToInt(lvl2c / 60), Mathf.FloorToInt(lvl2c % 60));
 
             checkPoint = new Vector3(135, 33, -2);
             ball.transform.position = checkPoint;
-            timeRemaining = 120f + lvl2;
+            //new timer for current lvl
+            timeRemaining = 120f + timeRemaining;
             curLvl = 5;
         }   
         if (level == 7)
         {
-            lvl3 = timeRemaining;
-            ExtraTime3.text = string.Format("+ {0:00}:{1:00}", Mathf.FloorToInt(lvl3 / 60), Mathf.FloorToInt(lvl3 % 60));
+            //time completed previous level
+            lvl3c = 120f - timeRemaining;
+            //bonus time
+            lvl3b = timeRemaining;
+            levelTime3.text = string.Format("{0:00}:{1:00}", Mathf.FloorToInt(lvl3c / 60), Mathf.FloorToInt(lvl3c % 60));
+            timer.SetActive(false);
             //create stats display UI here
         }
         if(level == 8)
@@ -256,7 +275,7 @@ public class BallController : MonoBehaviour
         if (timeRemaining > 0.05f)
         {
             timeRemaining -= Time.deltaTime;
-            timer.text = string.Format("{0:00}:{1:00}", Mathf.FloorToInt(timeRemaining / 60), Mathf.FloorToInt(timeRemaining % 60));
+            timerText.text = string.Format("{0:00}:{1:00}", Mathf.FloorToInt(timeRemaining / 60), Mathf.FloorToInt(timeRemaining % 60));
         }
         else //if (timeRemaining <= 0.01f)
         {
